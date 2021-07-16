d940ab5805410167e2d2115940575d9a4579a70b
nicolas <feigen@bzz.ch>
2021-07-16 06:11:07 -0700

    added Seeder but not implementet the datq

--- src/main/java/ch/zli/m223/punchclock/seeder/DatabaseSeeder.java
+++ src/main/java/ch/zli/m223/punchclock/seeder/DatabaseSeeder.java
@@ -1,4 +1,29 @@
 package ch.zli.m223.punchclock.seeder;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DatabaseSeeder {
+
+    @Autowired
+    public DatabaseSeeder(){
+
+    }
+
+    @EventListener
+    public void seed(ContextRefreshedEvent event) {
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+        System.out.println("1111");
+
+    }
 }
